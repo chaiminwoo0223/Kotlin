a22d57f667c2a108d79aed0fcd05e1dd1bc6ffd0
최민우 <chaiminwoo0223@naver.com>
2024-10-01 18:14:54 +0900

    고차 함수와 람다식

---
+++ src/chap03/section2/CallByName.kt
@@ -0,0 +1,17 @@
+package chap03.section2
+
+// 람다식 이름을 사용해 호출하기
+fun main() {
+    val result = callByName(otherLambda) // 람다식 이름으로 호출
+    println(result)
+}
+
+fun callByName(b: () -> Boolean): Boolean { // 람다식 자료형으로 선언된 매개변수
+    println("callByName function")
+    return b()
+}
+
+val otherLambda: () -> Boolean = {
+    println("otherLambda function")
+    true
+}
---
+++ src/chap03/section2/CallByValue.kt
@@ -0,0 +1,17 @@
+package chap03.section2
+
+// 값에 의한 호출로 람다식 사용하기
+fun main() {
+    val result = callByValue(lambda()) // 람다식 함수를 호출
+    println(result)
+}
+
+fun callByValue(b: Boolean): Boolean { // 일반 변수 자료형으로 선언된 매개변수
+    println("callByValue function")
+    return b
+}
+
+val lambda: () -> Boolean = {
+    println("lambda function")
+    true
+}
